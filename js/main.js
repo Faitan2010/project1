71b7c8e37c8210f3af60a9fd742a94ef0b210070
Faitan2010 <jenia1800@gmail.com>
2022-09-18 22:20:43 +0300

    add two another sliders

--- js/main.js
+++ js/main.js
@@ -18,3 +18,30 @@ $(document).ready(function () {
 </svg>`
     });
 });
+$(document).ready(function () {
+    $('.team-slayder').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: `<svg class="slayder-arrow team-prev" width="19" height="32" viewBox="0 0 19 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M17 2L3 16L17 30" stroke="#FFFFFF" stroke-width="3" />
+</svg>`,
+        nextArrow: ` <svg class="slayder-arrow team-next" width="19" height="32" viewBox="0 0 19 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M2 2L16 16L2 30" stroke="#FFFFFF" stroke-width="3" />
+</svg>`
+    });
+});
+$(document).ready(function () {
+    $('.rewiev-slayder').slick({
+        prevArrow: `<svg class="slayder-arrow rewiev-prev" width="19" height="32" viewBox="0 0 19 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M17 2L3 16L17 30" stroke="#333333" stroke-width="3" />
+</svg>`,
+        nextArrow: ` <svg class="slayder-arrow rewiev-next" width="19" height="32" viewBox="0 0 19 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M2 2L16 16L2 30" stroke="#333333" stroke-width="3" />
+</svg>`,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 2,
+    variableWidth: true
+    });
+});
