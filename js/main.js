ab41e3ec42a49876c66c6e6c84c5d7abe299a185
a.sivak <riziy_bes@mail.ru>
2022-09-19 14:38:52 +0200

    animate header

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@
 // 8 - animate icons
 // 9 - wow.js
 
+// window.addEventListener('load', () => {})
+
 $(document).ready(function () {
     $('.slayder').slick({
         prevArrow: `<svg class="slayder-arrow prev" width="19" height="32" viewBox="0 0 19 32" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -17,8 +19,7 @@ $(document).ready(function () {
     <path d="M2 2L16 16L2 30" stroke="#333333" stroke-width="3" />
 </svg>`
     });
-});
-$(document).ready(function () {
+
     $('.team-slayder').slick({
         infinite: true,
         slidesToShow: 3,
@@ -30,8 +31,7 @@ $(document).ready(function () {
     <path d="M2 2L16 16L2 30" stroke="#FFFFFF" stroke-width="3" />
 </svg>`
     });
-});
-$(document).ready(function () {
+
     $('.rewiev-slayder').slick({
         prevArrow: `<svg class="slayder-arrow rewiev-prev" width="19" height="32" viewBox="0 0 19 32" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M17 2L3 16L17 30" stroke="#333333" stroke-width="3" />
@@ -39,9 +39,30 @@ $(document).ready(function () {
         nextArrow: ` <svg class="slayder-arrow rewiev-next" width="19" height="32" viewBox="0 0 19 32" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M2 2L16 16L2 30" stroke="#333333" stroke-width="3" />
 </svg>`,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 2,
-    variableWidth: true
+        infinite: false,
+        speed: 300,
+        slidesToShow: 2,
+        variableWidth: true
     });
+    const headerMenu = document.querySelector('.header-menu');
+    let prevScrollValue = window.scrollY;
+
+    window.addEventListener('scroll', animateHeader);
+
+    function animateHeader () {
+        if (prevScrollValue < window.scrollY) {
+
+            if (!headerMenu.classList.contains('header-animate')) {
+                headerMenu.classList.add('header-animate');
+            }
+        } else {
+
+            if (headerMenu.classList.contains('header-animate')) {
+                headerMenu.classList.remove('header-animate');
+            }
+        }
+
+        prevScrollValue = window.scrollY;
+    }
 });
+
