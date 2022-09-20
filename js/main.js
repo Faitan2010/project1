e7137e9e3140f4a43968315c467387bef290f510
Faitan2010 <jenia1800@gmail.com>
2022-09-20 12:01:58 +0300

    add loader

--- js/main.js
+++ js/main.js
@@ -11,6 +11,10 @@
 // window.addEventListener('load', () => {})
 
 $(document).ready(function () {
+    const mask = document.querySelector('.mask')
+        setTimeout(() => {
+            mask.classList.add ('mask-none');
+        }, 1200)
     $('.slayder').slick({
         prevArrow: `<svg class="slayder-arrow prev" width="19" height="32" viewBox="0 0 19 32" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M17 2L3 16L17 30" stroke="#333333" stroke-width="3" />
