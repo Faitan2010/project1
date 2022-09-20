c748442ae29765d03b881b4c427ec676114b94f5
a.sivak <riziy_bes@mail.ru>
2022-09-20 15:17:41 +0200

    add base counters implementation

--- js/main.js
+++ js/main.js
@@ -12,9 +12,9 @@
 
 $(document).ready(function () {
     const mask = document.querySelector('.mask')
-        setTimeout(() => {
-            mask.classList.add ('mask-none');
-        }, 1200)
+    setTimeout(() => {
+        mask.classList.add('mask-none');
+    }, 1200)
     $('.slayder').slick({
         prevArrow: `<svg class="slayder-arrow prev" width="19" height="32" viewBox="0 0 19 32" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M17 2L3 16L17 30" stroke="#333333" stroke-width="3" />
@@ -53,7 +53,7 @@ $(document).ready(function () {
 
     window.addEventListener('scroll', animateHeader);
 
-    function animateHeader () {
+    function animateHeader() {
         if (prevScrollValue < window.scrollY) {
 
             if (!headerMenu.classList.contains('header-animate')) {
@@ -68,5 +68,45 @@ $(document).ready(function () {
 
         prevScrollValue = window.scrollY;
     }
+
+    const counterWrappers = [...document.querySelectorAll('.table-column-counter-info')];
+
+    counterWrappers.forEach((item) => {
+        counter(item);
+    });
 });
 
+function counter(counter) {
+    if (!counter) {
+        return;
+    }
+
+    this.plus = counter.querySelector('.span-plus');
+    this.minus = counter.querySelector('.span-minus');
+    const counterInput = counter.querySelector('.table-column-counter-info-text');
+
+    this.increment = (e) => {
+        e.preventDefault();
+
+        counterInput.value = Number(counterInput.value) + 1;
+    }
+
+    this.decrement = (e) => {
+        e.preventDefault();
+
+        if (counterInput.value <= 0) {
+            return
+        }
+        counterInput.value = Number(counterInput.value) - 1;
+    }
+
+    this.checkInputValue = (e) => {
+        if (isNaN(counterInput.value)) {
+            counterInput.value = 0;
+        }
+    }
+
+    this.plus.addEventListener('click', this.increment);
+    this.minus.addEventListener('click', this.decrement);
+    counterInput.addEventListener('input', this.checkInputValue);
+}
